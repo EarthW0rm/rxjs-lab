263cb5192f249e09e89af0590179af9bdf3c144e
earthw0rm <marco.a.faustino@gmail.com>
2020-01-25 10:36:08 -0300

    Adicionando o teste do operador Map

--- rxjs-lab-workspace/projects/lab/src/lib/operators/map/map-exemple.service.spec.ts
+++ rxjs-lab-workspace/projects/lab/src/lib/operators/map/map-exemple.service.spec.ts
@@ -1,12 +1,42 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { FilmsService } from '../../swapi/films.service';
+import { map } from 'rxjs/operators';
 
-import { MapExempleService } from './map-exemple.service';
+describe('Operador - Map', () => {
+	beforeEach(() => TestBed.configureTestingModule({
+		imports: [
+			HttpClientModule
+		],
+		providers: [
+			{ provide: 'SWAPI_URL', useValue: 'https://swapi.co/api/' }
+		]
+	}));
 
-describe('MapExempleService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+	it('should be created', () => {
+		const service: FilmsService = TestBed.get(FilmsService);
+		expect(service).toBeTruthy();
+	});
 
-  it('should be created', () => {
-    const service: MapExempleService = TestBed.get(MapExempleService);
-    expect(service).toBeTruthy();
-  });
+	it('deve mapear o nome do primeiro filme da saga', done => {
+		const service: FilmsService = TestBed.get(FilmsService);
+		const subscription$ = service.GetFilm(1)
+			.pipe(
+				map(_ => _.title)
+			)
+			.subscribe(
+				next => {
+					expect(next).toBe('A New Hope'); 
+					console.log(`O Primeiro filme da série é: ${next}`);
+				},
+				error => { 
+					console.error(error); 
+				},
+				() => { 
+					subscription$.unsubscribe();
+
+					done();
+				}
+			);
+	});
 });
