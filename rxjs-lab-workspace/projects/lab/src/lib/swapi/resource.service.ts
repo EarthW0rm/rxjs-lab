7cd126e7962ac57844c58e69907f06832a53718c
Marco Faustino <marco_faustino@hotmail.com>
2020-01-25 02:19:44 -0300

    feat: Adicionando service para busca de sub-itens

---
+++ rxjs-lab-workspace/projects/lab/src/lib/swapi/resource.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PagedResults } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResourceService {
+	constructor(private http: HttpClient ) { }
+
+	public GetPagedResults<T>(methodUri: string): Observable<PagedResults<T>> {
+		const _observable = this.http.get<PagedResults<T>>(methodUri, { headers: {'Content-Type': 'application/json'}});
+		return _observable;
+	}
+
+	public GetItem<T>(methodUri: string): Observable<T> {
+		const _observable = this.http.get<T>(methodUri, { headers: {'Content-Type': 'application/json'}});
+		return _observable;
+	}
+}
