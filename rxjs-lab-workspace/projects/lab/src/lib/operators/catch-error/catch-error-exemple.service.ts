9904b428428dd8be6165068dad26f3e44bb2026f
Marco Faustino <marco_faustino@hotmail.com>
2020-01-25 01:55:28 -0300

    Adicionando o teste do catchError

--- rxjs-lab-workspace/projects/lab/src/lib/operators/catch-error/catch-error-exemple.service.ts
+++ rxjs-lab-workspace/projects/lab/src/lib/operators/catch-error/catch-error-exemple.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { PagedResults, Film, JsendEnvelope } from '../../models';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatchErrorExempleService {
+  constructor(@Inject('SWAPI_URL') private swapiUrl: string, private http: HttpClient ) { }
 
-  constructor() { }
+	public GetFilms(): Observable<JsendEnvelope<PagedResults<Film>>> {
+
+		const methodUri = `${this.swapiUrl}films`;
+
+		const _observable = this.http.get<PagedResults<Film>>(methodUri, { headers: {'Content-Type': 'application/json'}}).pipe(
+			map((_: PagedResults<Film>) => { return { data: _, status: 'success'} as JsendEnvelope<PagedResults<Film>>})
+		); 
+
+		return _observable;
+	}
 }
