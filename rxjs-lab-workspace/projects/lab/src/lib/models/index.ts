accac0d5633e1a00182f80d31f93cfa97de6a32c
Marco Faustino <marco_faustino@hotmail.com>
2020-01-25 02:31:18 -0300

    fix: Alterando nemde de models

--- rxjs-lab-workspace/projects/lab/src/lib/models/index.ts
+++ rxjs-lab-workspace/projects/lab/src/lib/models/index.ts
@@ -4,5 +4,5 @@ export * from './paged-results';
 export * from './people';
 export * from './planet';
 export * from './specie';
-export * from './starship';
+export * from './spaceship';
 export * from './vehicle';
