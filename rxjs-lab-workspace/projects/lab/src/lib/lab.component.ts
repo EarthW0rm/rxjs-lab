ef3c88bae2eb85f7b44f6d44772adeecf8f10c08
Marco Faustino <marco_faustino@hotmail.com>
2020-01-24 23:19:33 -0300

    feat: Adicionando o projeto angular library para teste dos comandos

---
+++ rxjs-lab-workspace/projects/lab/src/lib/lab.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-lab',
+  template: `
+    <p>
+      lab works!
+    </p>
+  `,
+  styles: []
+})
+export class LabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
