7fa195e7bc97882b7c2e711e5499a87c3846c06b
Marco Faustino <marco_faustino@hotmail.com>
2020-01-25 02:20:28 -0300

    feat: Adicionando services para acesso aos métodos do swapi

---
+++ rxjs-lab-workspace/projects/lab/src/lib/swapi/peoples.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ResourceService } from './resource.service';
+import { PagedResults, People } from '../models';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class PeoplesService {
+	constructor(@Inject('SWAPI_URL') private swapiUrl: string, private resourceService: ResourceService) { }
+
+	public GetPeoples(): Observable<PagedResults<People>> {
+		let methodUri = `${this.swapiUrl}people`;
+
+		const _observable = this.resourceService.GetPagedResults<People>(methodUri);
+		return _observable;
+	}
+
+	public GetPeople(peopleId: number): Observable<People> {
+		let methodUri = `${this.swapiUrl}people/${peopleId}`;
+
+		const _observable = this.resourceService.GetItem<People>(methodUri);
+		return _observable;
+	}
+}
