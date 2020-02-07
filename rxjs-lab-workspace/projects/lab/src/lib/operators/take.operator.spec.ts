d240e17f208abff3dabe346398613825aa9352b8
Marco Faustino <marco.faustino@viajarbarato.com.br>
2020-02-07 14:11:56 -0300

    feat: Adicionando o teste do perator Take

--- rxjs-lab-workspace/projects/lab/src/lib/operators/take.operator.spec.ts
+++ rxjs-lab-workspace/projects/lab/src/lib/operators/take.operator.spec.ts
@@ -1,9 +1,39 @@
 import { TestBed } from '@angular/core/testing';
+import { SwapiModule, FilmsService } from '../swapi';
+import { take, switchMap, count } from 'rxjs/operators';
 
 describe('Take', () => {
-	beforeEach(() => TestBed.configureTestingModule({}));
+	beforeEach(() =>
+		TestBed.configureTestingModule({
+			imports: [SwapiModule.forRoot()]
+		})
+	);
 
-	it('should be created', () => {
-		expect(true).toBeTruthy();
+	it('Utilizando o método take para responder apenas o nome dos 2 primeiros filme da série', done => {
+		let countNexts = 0;
+
+		// Carregando a instancia do serviço FilmsService.
+		const filmsService: FilmsService = TestBed.get(FilmsService);
+
+		const subscription$ = filmsService
+			.GetFilms()
+			.pipe(
+				switchMap(_ => _.results),
+				take(2)
+			)
+			.subscribe(
+				next => {
+					countNexts += 1;
+					console.log(`O Nome do filme é: ${next.title}`);
+				},
+				error => {
+					console.error(error);
+				},
+				() => {
+					subscription$.unsubscribe();
+					expect(countNexts).toBe(2);
+					done();
+				}
+			);
 	});
 });
